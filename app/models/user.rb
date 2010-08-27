2d473fa518bf4809c6b67eb83124cd692b3d8ed1
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-08-27 00:22:32 -0300

    More cleanup, removing scaffold css, blank lines, unuseful methods (for now)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,11 @@
 class User < ActiveRecord::Base
-
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name
-  
+
   has_many :posts
   has_many :meetings
 
   validates_format_of :full_name, :with => /^([\w\d]+ [\w\d]+)+$/
-
-  def first_name
-    self.full_name.split.first
-  end
-
-  def last_name
-    self.full_name.split.last
-  end
-
 end
