636de9b88dc64fc1d99282941160a8c728335423
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-08-27 00:11:25 -0300

    Update gems, and some cleanup in views

--- db/migrate/20100803022044_create_users.rb
+++ db/migrate/20100803022044_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :full_name
-      t.string :email
       t.database_authenticatable :null => false
       t.recoverable
       t.rememberable
@@ -11,10 +10,10 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-    
+
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    
+
   end
 
   def self.down
