376c1df2e1e1f2afd9f1789e4fc2056b7f8082bb
Marcelo Griggio Cajueiro <marcelocajueiro@gmail.com>
2010-09-03 11:33:25 -0300

    member add/remove

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,10 @@ module ApplicationHelper
   def textilize(text)
     RedCloth.new(text).to_html.html_safe
   end
+  
+  def gravatar_image_tag(email, html_options = {})
+    email = Digest::MD5.hexdigest(email)
+    image_tag "http://www.gravatar.com/avatar/#{email}?size=48", html_options
+  end
+  
 end
