02f21f0a4e173220f3aceccc45fb846ef37f0888
Marcelo Griggio Cajueiro <marcelocajueiro@gmail.com>
2010-09-09 00:08:37 -0300

    comma

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ User.create!(
   :full_name => "Gurusc Admin", 
   :email => "admin@guru-sc.org", 
   :password => "123456", 
-  :password_confirmation => "123456"
+  :password_confirmation => "123456",
   :role => "admin"
 )
