957a47abfc2e5b5158efae2722b66cfc93de0a22
Marcelo Griggio Cajueiro <marcelocajueiro@gmail.com>
2010-09-17 21:19:42 -0300

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100913201824) do
+ActiveRecord::Schema.define(:version => 20100917214710) do
 
   create_table "comments", :force => true do |t|
     t.string   "author"
@@ -22,9 +22,17 @@ ActiveRecord::Schema.define(:version => 20100913201824) do
     t.datetime "updated_at"
   end
 
+  create_table "contacts", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "subject"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "meetings", :force => true do |t|
     t.string   "title"
-    t.date     "date"
     t.string   "autor"
     t.text     "description"
     t.string   "code"
@@ -36,6 +44,8 @@ ActiveRecord::Schema.define(:version => 20100913201824) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.datetime "date"
+    t.string   "local"
   end
 
   create_table "post_categories", :force => true do |t|
@@ -72,6 +82,17 @@ ActiveRecord::Schema.define(:version => 20100913201824) do
   create_table "projects_users", :id => false, :force => true do |t|
     t.integer "user_id",    :null => false
     t.integer "project_id", :null => false
+  end
+
+  create_table "uploads", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "document_file_name"
+    t.string   "document_content_type"
+    t.integer  "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   create_table "users", :force => true do |t|
